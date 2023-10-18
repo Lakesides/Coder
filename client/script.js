0bdd95bf1b769010e43f147a9cd1553c976866c2
coder <coder@exapmple.com>
2023-10-18 15:19:30 -0700

    second commit

--- client/script.js
+++ client/script.js
@@ -114,7 +114,7 @@ form.addEventListener("keyup", (e) => {
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 30) {
+    if (window.scrollY > 20) {
         toTop.classList.add("active");
     } else {
         toTop.classList.remove("active");
