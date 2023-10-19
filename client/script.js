416113e141c244d66e01694132753cba9acea9e8
coder <coder@exapmple.com>
2023-10-18 18:22:22 -0700

    final commit

--- client/script.js
+++ client/script.js
@@ -111,15 +111,7 @@ form.addEventListener("keyup", (e) => {
   }
 });
 
-const toTop = document.querySelector(".to-top");
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 20) {
-        toTop.classList.add("active");
-    } else {
-        toTop.classList.remove("active");
-    }
-})
 
-const time = document.querySelector(".time");
+
 
