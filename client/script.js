2b2c021d2e2d967f55b3bb184773ba85ae87280e
coder <coder@exapmple.com>
2024-03-24 10:38:21 +0100

    commit new changes

--- client/script.js
+++ client/script.js
@@ -112,6 +112,31 @@ form.addEventListener("keyup", (e) => {
 });
 
 
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
 
+  loader.classList.add("loader-hidden");
+
+  loader.addEventListener("transitionend", () => {
+      document.body.removeChild("loader");
+  })
+})
+
+const cross = document.querySelector(".cross");
+const popUp = document.querySelector(".pop-upp");
+const submitBtn = document.querySelector(".submit");
+const popupBtn = document.querySelector("#button");
+
+cross.addEventListener("click", ()=> {
+  popUp.classList.remove("visid");
+})
+
+submitBtn.addEventListener("click", ()=> {
+  popUp.classList.remove("visid");
+})
+
+popupBtn.addEventListener("click", ()=> {
+  popUp.classList.add("visid");
+})
 
 
